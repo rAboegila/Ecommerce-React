6c82982bab322026b56689276b2f48c7e5f97124
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-05-06 10:24:04 +0300

    fixed carousel bug + removed unnecessary files

--- src/Componets/Product/Product.js
+++ src/Componets/Product/Product.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 import './Product.css';
 
-//Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
 
 export default function Product() {
  
