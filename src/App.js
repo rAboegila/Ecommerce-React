016d6d98a3fff7bb6933b5ceb0d05879ab6a0339
Rawan <rawan.moyasser.aboegila@gmail.com>
2023-05-21 03:16:10 +0300

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -7,61 +7,62 @@ import "./App.css";
 import Products from "./Pages/Products/Products";
 import Home from "./Pages/Home/Home";
 import NotFound from "./Pages/404/NotFound";
+import WishList from "./Pages/WishList/Wishlist";
 
 // Components
-import Navbar from './Componets/Navbar/Navbar';
-import Footer from './Componets/Footer/Footer';
-import AdminProducts from './Admin/AdminProducts/AdminProducts';
-import AddProduct from './Admin/AddProduct/AddProduct';
-import ProductDetails from './Componets/ProducDetails/ProductDetails';
-import Register from './Componets/Register/Register';
-import Login from './Componets/Login/Login';
-import EditProduct from './Admin/Edit Product/EditProduct';
-import Newlogin from './Componets/Login/Newlogin';
+import Navbar from "./Componets/Navbar/Navbar";
+import Footer from "./Componets/Footer/Footer";
+import AdminProducts from "./Admin/AdminProducts/AdminProducts";
+import AddProduct from "./Admin/AddProduct/AddProduct";
+import ProductDetails from "./Componets/ProducDetails/ProductDetails";
+import Register from "./Componets/Register/Register";
+import Login from "./Componets/Login/Login";
+import EditProduct from "./Admin/Edit Product/EditProduct";
+import Newlogin from "./Componets/Login/Newlogin";
 import Admin from "./Admin/Admin/Admin";
 import ProtectedRoutes from "./Lib/ProtectedRoutes";
 import ProtectedUserRoutes from "./Lib/ProtectedUserRoute";
 
-
 export default function App() {
-  
   return (
     <div className="app">
       <Navbar />
-        <Routes>
+      <Routes>
         <Route
-            element={<ProtectedRoutes requiresLogin={true} redirectTo="/" />}
-          >
-              <Route path="adminproducts" element={<AdminProducts />} />
-              <Route path="products/add" element={<AddProduct />} />
-              <Route path="/product/update/:productId" element={<EditProduct />} />
-              <Route path="/admin" element={<Admin />} />
+          element={<ProtectedRoutes requiresLogin={true} redirectTo="/" />}
+        >
+          <Route path="adminproducts" element={<AdminProducts />} />
+          <Route path="products/add" element={<AddProduct />} />
+          <Route path="/product/update/:productId" element={<EditProduct />} />
+          <Route path="/admin" element={<Admin />} />
+        </Route>
 
-          </Route>
-
+        <Route
+          element={
+            <ProtectedUserRoutes requiresLogin={true} redirectTo="/login" />
+          }
+        >
           <Route
-            element={<ProtectedUserRoutes requiresLogin={true} redirectTo="/login" />}
-          >
+            exact
+            path="product/:productId/"
+            element={<ProductDetails />}
+          />
+          <Route path="/products" element={<Products />} />
+          <Route path="/wishlist" element={<WishList />} />
+        </Route>
 
-          <Route exact path="product/:productId/" element={<ProductDetails />} />
-              <Route path="/products" element={<Products />} />
-          </Route>
+        <Route
+          element={<ProtectedUserRoutes requiresLogin={false} redirectTo="/" />}
+        >
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Newlogin />} />
+        </Route>
 
-
-          <Route
-            element={<ProtectedUserRoutes requiresLogin={false} redirectTo="/" />}
-          >
-
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Newlogin />} />
-          </Route>
-
-          
-
-              <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    
-  <Footer />
+
+      <Footer />
     </div>
   );
 }
--- src/Pages/404/NotFound.js
+++ src/Pages/404/NotFound.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./NotFound.css";
 
+// export default function NotFound() {
+//   return <div className="bg-light p-5">NotFound</div>;
+// }
+import { Button, Result } from "antd";
 export default function NotFound() {
-  return <div className="bg-light p-5">NotFound</div>;
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button onClick={handleClick} type="primary">
+          Back Home
+        </Button>
+      }
+    />
+  );
 }
